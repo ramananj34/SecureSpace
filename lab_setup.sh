f68e86366fd3bbe0e7952bac1676941367d54236
Jude Ramanan <juder.ramanan@gmail.com>
2026-05-14 17:00:31 -0400

    Lab Setup

---
+++ lab_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+#Load secrets
+if [! -f secrets.sh]; then
+    echo "ERROR: secrets.sh not found."
+    exit 1
+fi
+source secrets.sh
+
+#Git config
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_USERNAME"
+git remote set-url origin "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git"
+
+#Conda env
+conda env create -f environment.yml
+conda run -n amrcc python -m ipykernel install --user --name securespace --display-name "Python (securespace)"
